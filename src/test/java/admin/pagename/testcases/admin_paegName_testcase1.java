c6393cc391e980d112f014704ad1cf37bedfe5a8
unknown <shakava@WKWIN2151832.global.publicisgroupe.net>
2020-01-30 15:43:30 +0530

    dummmy changes

--- src/test/java/admin/pagename/testcases/admin_paegName_testcase1.java
+++ src/test/java/admin/pagename/testcases/admin_paegName_testcase1.java
@@ -12,6 +12,7 @@ public class admin_paegName_testcase1 {
 			System.out.println("This is testing");
 		}catch(Custom_Exception e){
 			System.out.println("Custom error generated");
+			System.out.println("Custom error generated");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
